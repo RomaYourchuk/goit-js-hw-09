4a5b6c0c7b643918463fa2ead7a0d7157c4930d1
RomaYourchuk <yourchuk922@gmail.com>
2022-03-28 16:33:09 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+stopBtn.disabled = true;
+
+function changeBodyBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+    timerId = setInterval(changeBodyBackgroundColor, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+}
+
+function onStopClick() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
